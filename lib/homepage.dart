71136b4cbf07e70592e6cd85b9c5275ecc1fb97e
rahulg221 <125697931+rahulg221@users.noreply.github.com>
2023-08-10 14:49:57 -0400

    Add files via upload

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -16,12 +16,16 @@ class _MyHomePageState extends State<MyHomePage> {
   //Logs
   List<Map<String, dynamic>> _logs = [];
   bool _isLoading = true;
-  List<double> weeklyLogs = [180, 179, 181];
 
   Color primaryColor = Color.fromARGB(255, 255, 111, 0);
   Color secondaryColor = Color.fromARGB(255, 255, 125, 49);
 
   String fontStyle = 'Arvo';
+
+  double weeklyAvg = 0.0;
+
+  final TextEditingController _weightController = TextEditingController();
+  final TextEditingController _noteController = TextEditingController();
 
   //Fetches data from database
   void _refreshLogs() async {
@@ -32,26 +36,58 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Future<void> _addLog() async {
+    double weight = double.tryParse(_weightController.text) ?? 0.0;
+    String notes = _noteController.text;
+    await SQLHelper.addLog(weight, notes);
+    _refreshLogs();
+  }
+
+  void _deleteLog(int id) async {
+    await SQLHelper.deleteLog(id);
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Log deleted.',
+            style: TextStyle(
+                fontFamily: fontStyle, color: Colors.black, fontSize: 18)),
+        backgroundColor: secondaryColor));
+
+    _refreshLogs();
+  }
+
+  Future<void> _calculateAvg() async {
+    double avg = await SQLHelper.calculateAverageWeight();
+
+    setState(() {
+      weeklyAvg = avg;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     _refreshLogs(); // Load in all logs
   }
 
-  final TextEditingController _weightController = TextEditingController();
+  @override
+  void dispose() {
+    _noteController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
 
   void _showForm(int? id) async {
     // update existing log
     if (id != null) {
       final existingLog = _logs.firstWhere((element) => element['id'] == id);
       _weightController.text = existingLog['weight'];
+      _noteController.text = existingLog['notes'];
     } else {
       showModalBottomSheet(
           context: context,
           elevation: 5,
           isScrollControlled: true,
           builder: (_) => Container(
-              color: Colors.black,
+              color: secondaryColor,
               padding: EdgeInsets.only(
                 top: 15,
                 left: 15,
@@ -67,7 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     keyboardType:
                         TextInputType.numberWithOptions(decimal: true),
                     decoration: InputDecoration(
-                      hintText: 'Enter weight',
+                      hintText: 'Weight',
                       hintStyle: TextStyle(fontFamily: fontStyle),
                       contentPadding: EdgeInsets.symmetric(
                           vertical: 15.0, horizontal: 20.0), // Adjust padding
@@ -79,74 +115,155 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                   const SizedBox(height: 10),
-                  TextButton(
+                  TextField(
+                    controller: _noteController,
+                    maxLines: 3,
+                    decoration: InputDecoration(
+                      hintText: 'Notes',
+                      hintStyle: TextStyle(fontFamily: fontStyle),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 20.0), // Adjust padding
+                      filled: true,
+                      fillColor: Colors.white, // Set the background color
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white),
                     onPressed: () async {
                       await _addLog();
 
-                      _weightController.text = '';
+                      _calculateAvg();
+
+                      _weightController.clear();
+                      _noteController.clear();
 
                       if (!mounted) return;
                       Navigator.of(context).pop();
                     },
                     child: Text('Add',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontFamily: fontStyle)),
+                        style: TextStyle(fontSize: 20, fontFamily: fontStyle)),
                   )
                 ],
               )));
     }
   }
 
-  Future<void> _addLog() async {
-    double weight = double.tryParse(_weightController.text) ?? 0.0;
-    await SQLHelper.addLog(weight);
-    _refreshLogs();
-  }
+  Widget _logView() => Container(
+        height: MediaQuery.of(context).size.height * 0.7,
+        child: ListView.builder(
+          itemCount: _logs.length,
+          itemBuilder: (context, index) {
+            final weight = _logs[index]['weight'] as double;
+            final notes = _logs[index]['notes'] ?? '';
+            final currentDate = DateTime.parse(_logs[index][
+                'currentDate']); // Assuming you're storing the date as a String
+            final formattedDate =
+                DateFormat('MMM dd, yyyy').format(currentDate);
 
-  void _deleteLog(int id) async {
-    await SQLHelper.deleteLog(id);
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text('Log deleted.', style: TextStyle(fontFamily: fontStyle)),
-      backgroundColor: Colors.black,
-    ));
-
-    _refreshLogs();
-  }
-
-  Widget _logView() => ListView.builder(
-        itemCount: _logs.length,
-        itemBuilder: (context, index) {
-          final weight = _logs[index]['weight'] as double;
-          final currentDate = DateTime.parse(_logs[index]
-              ['currentDate']); // Assuming you're storing the date as a String
-          final formattedDate = DateFormat('MMM dd, yyyy').format(currentDate);
-
-          return Card(
-            color: secondaryColor,
-            margin: const EdgeInsets.all(15),
-            child: ListTile(
-                title: Text(weight.toString() + ' lbs',
+            return Card(
+              elevation: 3.0,
+              color: secondaryColor,
+              margin: const EdgeInsets.all(15),
+              child: GestureDetector(
+                onTap: () {
+                  if (notes != '') {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          notes,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontFamily: fontStyle,
+                          ),
+                        ),
+                        backgroundColor: secondaryColor,
+                      ),
+                    );
+                  }
+                },
+                child: ListTile(
+                  title: Text(
+                    weight.toString() + ' lbs',
                     style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black,
-                        fontSize: 25,
-                        fontFamily: fontStyle)),
-                subtitle: Text(formattedDate,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontFamily: fontStyle,
+                    ),
+                  ),
+                  subtitle: Text(
+                    formattedDate,
                     style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontFamily: fontStyle)),
-                trailing: SizedBox(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontFamily: fontStyle,
+                    ),
+                  ),
+                  trailing: SizedBox(
                     width: 30,
                     child: IconButton(
-                      icon: const Icon(Icons.clear, color: Colors.black),
-                      onPressed: () => _deleteLog(_logs[index]['id']),
-                    ))),
-          );
-        },
+                        icon: const Icon(Icons.clear, color: Colors.black),
+                        onPressed: () {
+                          _deleteLog(_logs[index]['id']);
+
+                          _calculateAvg();
+                        }),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      );
+
+  Widget _dashboard() => Material(
+        elevation: 3.0, // Adjust the elevation value as needed
+        borderRadius: BorderRadius.circular(15.0),
+        child: Container(
+            height: MediaQuery.of(context).size.height * 0.15,
+            width: MediaQuery.of(context).size.width - 30,
+            decoration: BoxDecoration(
+              color: secondaryColor,
+              borderRadius:
+                  BorderRadius.circular(15.0), // Adjust the radius as needed
+            ),
+            child: Center(
+                child: Padding(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.02,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    '${weeklyAvg.toStringAsFixed(1)} lbs',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                      fontSize: 40,
+                      fontFamily: fontStyle,
+                    ),
+                  ),
+                  Text(
+                    'weekly avg.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontFamily: fontStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ))),
       );
 
   Widget build(BuildContext context) {
@@ -160,28 +277,15 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.black,
                 fontSize: 30,
                 fontFamily: fontStyle)),
-        actions: [
-          IconButton(
-            onPressed: () {
-              // Navigate to the other page
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => MyStatsPage(
-                          title: 'Stats',
-                          weeklyLogs: [1, 2, 3],
-                        )),
-              );
-            },
-            icon: Icon(Icons.insert_chart,
-                color: Colors.black,
-                size: 25), // Wrap the icon with Icon widget
-          )
-        ],
       ),
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
-          : _logView(),
+          : Column(
+              children: [
+                _dashboard(),
+                _logView(),
+              ],
+            ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _showForm(null),
         tooltip: 'Add Log',
--- lib/sql_helper.dart
+++ lib/sql_helper.dart
@@ -4,20 +4,27 @@ import 'package:sqflite/sqflite.dart' as sql;
 class SQLHelper {
   static Future<void> createTables(sql.Database database) async {
     await database.execute(
-        """CREATE TABLE items(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, weight REAL, currentDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)""");
+        """CREATE TABLE items(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, weight REAL, notes TEXT, currentDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)""");
   }
 
   static Future<sql.Database> db() async {
-    return sql.openDatabase('weightTracker.db', version: 1,
-        onCreate: (sql.Database database, int version) async {
-      await createTables(database);
-    });
+    return sql.openDatabase(
+      'weightTracker.db', version: 2, // Increment the version number
+      onCreate: (sql.Database database, int version) async {
+        await createTables(database);
+      },
+      onUpgrade: (sql.Database db, int oldVersion, int newVersion) async {
+        if (oldVersion < 2) {
+          await db.execute('ALTER TABLE items ADD COLUMN notes TEXT');
+        }
+      },
+    );
   }
 
-  static Future<int> addLog(double weight) async {
+  static Future<int> addLog(double weight, String notes) async {
     final db = await SQLHelper.db();
 
-    final data = {'weight': weight};
+    final data = {'weight': weight, 'notes': notes};
     final id = await db.insert('items', data,
         conflictAlgorithm: sql.ConflictAlgorithm.replace);
     return id;
@@ -34,6 +41,26 @@ class SQLHelper {
     return db.query('items', where: "id = ?", whereArgs: [id], limit: 1);
   }
 
+  static Future<List<Map<String, dynamic>>> getWeeklyLogs() async {
+    final db = await SQLHelper.db();
+    return db.query('items',
+        orderBy: "id DESC", limit: 7); // Fetch the last 7 entries
+  }
+
+  static Future<double> calculateAverageWeight() async {
+    final logs = await getWeeklyLogs();
+    if (logs.isEmpty) {
+      return 0.0; // Default value if there are no logs
+    }
+
+    double totalWeight = 0;
+    for (var log in logs) {
+      totalWeight += log['weight'] as double;
+    }
+
+    return totalWeight / logs.length;
+  }
+
   static Future<void> deleteLog(int id) async {
     final db = await SQLHelper.db();
 
@@ -47,6 +74,7 @@ class SQLHelper {
 
 //id: id of entry
 //weight: logged weight
+//notes: additional notes
 //currentDate: date logged on
 
 
